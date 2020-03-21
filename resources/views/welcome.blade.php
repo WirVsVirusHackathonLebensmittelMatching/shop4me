d2e036dd0a7a343fd8ed11c915b47a269f6a8114
Tri Nhan Vu <trinhanvu@gmail.com>
2020-03-21 16:27:06 +0100

    move layout to separate view and extend from it

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,150 +1,54 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.app')
+@section('content')
+    <div class="title m-b-md">
+        Starte dein Einkaufshilfe-Team <br>in deiner Stadt
+    </div>
+    <div class="row mb-5">
+        <div class="col-6 mx-auto">
 
-    <title>Laravel</title>
-
-    <!-- Fonts -->
-    <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
-          integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-
-    <!-- Styles -->
-    <style>
-        html, body {
-            background-color: #fff;
-            color: #636b6f;
-            font-family: 'Nunito', sans-serif;
-            font-weight: 200;
-            height: 100vh;
-            margin: 0;
-        }
-
-        .full-height {
-            height: 100vh;
-        }
-
-        .flex-center {
-            align-items: center;
-            display: flex;
-            justify-content: center;
-        }
-
-        .position-ref {
-            position: relative;
-        }
-
-        .top-right {
-            position: absolute;
-            right: 10px;
-            top: 18px;
-        }
-
-        .content {
-            text-align: center;
-        }
-
-        .title {
-            font-size: 84px;
-        }
-
-        .links > a {
-            color: #636b6f;
-            padding: 0 25px;
-            font-size: 13px;
-            font-weight: 600;
-            letter-spacing: .1rem;
-            text-decoration: none;
-            text-transform: uppercase;
-        }
-
-        .m-b-md {
-            margin-bottom: 30px;
-        }
-    </style>
-</head>
-<body>
-<div class="flex-center position-ref full-height">
-    @if (Route::has('login'))
-        <div class="top-right links">
-            @auth
-                <a href="{{ url('/home') }}">Home</a>
-            @else
-                <a href="{{ route('login') }}">Login</a>
-
-                @if (Route::has('register'))
-                    <a href="{{ route('register') }}">Register</a>
-                @endif
-            @endauth
-        </div>
-    @endif
-    <div class="content container">
-        <div class="title m-b-md">
-            Starte dein Einkaufshilfe-Team <br>in deiner Stadt
-        </div>
-        <div class="row mb-5">
-            <div class="col-6 mx-auto">
-
-                <form action="">
-                    <div class="form-group">
-                        <label for=""></label>
-                        <input type="text"
-                               class="form-control form-control-lg" name="" id="" aria-describedby="helpId"
-                               placeholder="PLZ">
-                        <small id="helpId" class="form-text text-muted">Gebe deine PLZ ein</small>
-                    </div>
-                    <input name="" id="" class="btn btn-primary" type="button" value="Einkaufshelfer-Team gründen">
-                </form>
-            </div>
-
-        </div>
-        <div class="row">
-            <div class="col-4">
-                <div class="card">
-                    <div class="card-body">
-                        <h5 class="card-title">Warum</h5>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of
-                            the card's content.</p>
-                        <a href="#" class="btn btn-primary">Go somewhere</a>
-                    </div>
+            <form action="">
+                <div class="form-group">
+                    <label for=""></label>
+                    <input type="text"
+                           class="form-control form-control-lg" name="" id="" aria-describedby="helpId"
+                           placeholder="PLZ">
+                    <small id="helpId" class="form-text text-muted">Gebe deine PLZ ein</small>
                 </div>
-            </div>
-            <div class="col-4">
-                <div class="card">
-                    <div class="card-body">
-                        <h5 class="card-title">Warum</h5>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of
-                            the card's content.</p>
-                        <a href="#" class="btn btn-primary">Go somewhere</a>
-                    </div>
-                </div>
-            </div>
-            <div class="col-4">
-                <div class="card">
-                    <div class="card-body">
-                        <h5 class="card-title">Warum</h5>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of
-                            the card's content.</p>
-                        <a href="#" class="btn btn-primary">Go somewhere</a>
-                    </div>
-                </div>
-            </div>
+                <input name="" id="" class="btn btn-primary" type="button" value="Einkaufshelfer-Team gründen">
+            </form>
         </div>
-
-        <footer>
-            <div class="row mt-5">
-                <div class="col links">
-                    <a href="https://laravel.com/docs">Impressum</a>
-                    <a href="https://wirvsvirushackathon.org/">WirVsVirusHackathon.org</a>
-                    <a href="/team">Team</a>
-                    <a href="https://github.com/WirVsVirusHackathonLebensmittelMatching/shop4me">GitHub</a>
-                </div>
-            </div>
-        </footer>
 
     </div>
-</div>
-</body>
-</html>
+    <div class="row">
+        <div class="col-4">
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">Warum</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of
+                        the card's content.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-4">
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">Warum</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of
+                        the card's content.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-4">
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">Warum</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of
+                        the card's content.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
