c80ad4e193edb72c3baf5e265488e2b2fe3ea8d5
Tri Nhan Vu <trinhanvu@gmail.com>
2020-03-21 20:55:38 +0100

    add additional columns

--- resources/views/cities/search-list.blade.php
+++ resources/views/cities/search-list.blade.php
@@ -4,19 +4,25 @@
     <table class="table">
         <thead>
         <tr>
-            <th></th>
-            <th></th>
-            <th></th>
+            <th>Ortsname</th>
+            <th>PLZ</th>
+            <th>Bundesland</th>
+            <th>Aktion</th>
         </tr>
         </thead>
         <tbody>
 
-        @foreach ($cities as $city)
+        @foreach ($cities as $key => $city)
             <tr>
-                <td scope="row">{{$city->city_name}}</td>
+                <td scope="row">{{$key+1}}</td>
+                <td>{{$city->city_name}}</td>
                 <td>{{$city->zip_code}}</td>
+                <td>{{$city->state}}</td>
             </tr>
         @endforeach
         </tbody>
+        <form method="">
+
+        </form>
     </table>
 @endsection
--- resources/views/errors/notfound.blade.php
+++ resources/views/errors/notfound.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="alert alert-warning" role="alert">
+    <div class="alert alert-danger" role="alert">
         <strong>Keine Stadt zu der Postleitzahl {{$zip_code}} gefunden</strong>
     </div>
 @endsection
