9c5b0bf12a4123aed16dfcd2e9b0d18a70e9fc59
Tri Nhan Vu <trinhanvu@gmail.com>
2020-03-22 14:01:26 +0100

    add mission and vision text

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,6 @@
 @extends('layouts.app')
 @section('content')
+    <section>
     <div class="row mb-5">
         <div class="col-6 mx-auto">
             <h1 class="text-center">
@@ -89,42 +90,90 @@
             </div>
         </div>
     </div>
+    </section>
     @guest
-
         @if($cities->count() > 0)
-            <div class="row mt-5">
-                <div class="col">
-                    <h3 class="text-center mb-5">Folgende Städte werden von Einkaufshelfern unterstützt.️</h3>
-                    <table class="table table-striped">
-                        <thead class="thead-light">
-                        <tr>
-                            <th>Stadt/Stadteil</th>
-                            <th>PLZ</th>
-                            <th>Bundesland</th>
-                            <th>Hotline</th>
-                            <th></th>
-                        </tr>
-                        </thead>
-                        <tbody>
-                        @foreach($cities as $city)
+            <section>
+                <div class="row mt-5">
+                    <div class="col">
+                        <h3 class="text-center mb-5">Folgende Städte werden von Einkaufshelfern unterstützt.️</h3>
+                        <table class="table table-striped">
+                            <thead class="thead-light">
                             <tr>
-                                <td scope="row">{{$city->city_name}}</td>
-                                <td>{{$city->zip_code}}</td>
-                                <td>{{$city->state}}</td>
-                                <td><a href="tel:{{$city->city_team->hotline}}">{{$city->city_team->hotline}}</a></td>
-                                <td>
-                                    <a class="btn btn-sm btn-success"
-                                       href="{{route('city-teams.view', ['id' => $city->id])}}"
-                                       title="Details zum Team{{$city->city_name}}">Details</a>
-                                </td>
+                                <th>Stadt/Stadteil</th>
+                                <th>PLZ</th>
+                                <th>Bundesland</th>
+                                <th>Hotline</th>
+                                <th></th>
                             </tr>
-                        @endforeach
-                        </tbody>
-                    </table>
+                            </thead>
+                            <tbody>
+                            @foreach($cities as $city)
+                                <tr>
+                                    <td scope="row">{{$city->city_name}}</td>
+                                    <td>{{$city->zip_code}}</td>
+                                    <td>{{$city->state}}</td>
+                                    <td><a href="tel:{{$city->city_team->hotline}}">{{$city->city_team->hotline}}</a>
+                                    </td>
+                                    <td>
+                                        <a class="btn btn-sm btn-success"
+                                           href="{{route('city-teams.view', ['id' => $city->id])}}"
+                                           title="Details zum Team{{$city->city_name}}">Details</a>
+                                    </td>
+                                </tr>
+                            @endforeach
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
-            </div>
+            </section>
 
         @endif
+        <section>
+            <div class="row mt-5">
+                <div class="col-5 mx-auto">
+                    <h2 class="text-center">Die Story</h2>
+                    <p class="text-center">Alles begann mit dem <a href="https://wirvsvirushackathon.org/"
+                                                                   title="Wir versus Virus Hackathon">WirVsVirusHackathon</a>.
+                        <br>Das Ergebnis seht ihr vor euren Augen.</p>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-4">
+                    <div class="card">
+                        <div class="card-body">
+                            <h4 class="card-title">Die Vision</h4>
+                            <p class="card-text"> Wir wollen Freiwilligen dabei helfen denjenigen, die dringend auf
+                                Hilfe angewiesen sind, diese auch schnellstmöglich zukommen zu lassen. Deswegen stellen
+                                wir mit Einkaufshilfe ein Werkzeug zur Verfügung, mit dem sich Ehrenamtliche mit
+                                mehreren Personen in ihrer Stadt, ihrem Dorf oder ihrer Kommune als
+                                Einkaufshilfe-Botschafter registrieren und den gesamten Prozess einfach verwalten
+                                können. Außerdem bekommen sie bei Einkaufshilfe Materialien wie Flyer und Plakate,
+                                HowTo-Anleitungen und weitere Informationen an die Hand, um ihre eigene
+                                Einkaufshilfe-Gruppe schnell und effektiv zu starten.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-8">
+                    <div class="card">
+                        <div class="card-body">
+                            <h4 class="card-title">Die Mission</h4>
+                            <p class="card-text"> Da ältere Menschen, denen neben den anderen Risikogruppen vor allem
+                                geholfen werden muss, häufig keinen Zugang zu den vielen digitalen Hilfsangeboten haben,
+                                will Shop4Me den vielen Helfenden ein leicht zu handhabendes und vor allem übertragbares
+                                Werkzeug an die Hand geben, mit dem sie selbstständig agieren und Kontakte herstellen
+                                können.</p>
+                            <p class="card-text">Wir bauen auf die Erfahrung von ehrenamtlichen Gruppen auf, wie sie
+                                sich in den letzten Tagen vielerorts in Deutschland gegründet haben. Die Expertise einer
+                                Münsteraner-Freiwilligengruppe dient Shop4Me vor allem als Fundament. Unser
+                                hauptsächliches Anliegen ist es, diese Art der Freiwilligenhilfe zu vereinheitlichen und
+                                dadurch ein flächendeckendes Netz zu etablieren, um möglichst viele Menschen versorgen
+                                zu können.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
     @endguest
 @endsection
 
