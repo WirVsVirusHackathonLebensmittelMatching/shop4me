56ef6bb3f7c6978fbb5a9f56e8c060e80b8bac76
Tri Nhan Vu <trinhanvu@gmail.com>
2020-03-22 19:12:05 +0100

    add comment to logo that it might break

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -23,7 +23,7 @@
 <div id="app">
     <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
         <div class="container">
-            <a class="navbar-brand" href="{{ url('/') }}"> ❤️
+            <a class="navbar-brand" href="{{ url('/') }}"> ❤️ things break! alpha alpha!
                 {{ config('app.name', 'Einkaufshelfer') }} 🛒
             </a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
