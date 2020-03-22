a13185276e7841a7e3d97bf860edfd62b548ebed
Tri Nhan Vu <trinhanvu@gmail.com>
2020-03-22 13:37:26 +0100

    display details for team in card

--- resources/views/city-teams/view.blade.php
+++ resources/views/city-teams/view.blade.php
@@ -1,22 +1,27 @@
 @extends('layouts.app')
 @section('content')
     <div class="row">
-        <div class="col">
-            <span>Einkaufshelfer-Team für:</span>
-            <h1>{{$city->city_name}}</h1>
-            <h1>{{$city->zip_code}}</h1>
-        </div>
-    </div>
-    <div class="row">
         <div class="col-8 mx-auto">
-            <h3><span class="badge badge-info">{{$city->city_team->getStatus()}}</span></h3>
-            <label for="hotline">Hotline</label>
-            <h3 class="hotline"><a href="tel:{{$city->city_team->hotline}}">{{$city->city_team->hotline}}</a></h3>
-            <label for="support_email">Support-Email</label>
-            <h3 class="hotline"><a href="mailto:{{$city->city_team->team_email}}">{{$city->city_team->team_email}}</a>
-            </h3>
-            <label for="support_email">Beschreibung</label>
-            <h3 class="hotline">{{$city->city_team->description}}</h3>
+            <div class="card">
+                <div class="card-header">
+                    <h2>Einkaufshelfer-Team für: {{$city->city_name}} {{$city->zip_code}}</h2>
+                </div>
+                <div class="card-body">
+                    <h4 class="card-title"></h4>
+                    <h3><span class="badge badge-info">{{$city->city_team->getStatus()}}</span></h3>
+                    <label for="hotline">Hotline</label>
+                    <h3 class="hotline"><a href="tel:{{$city->city_team->hotline}}">{{$city->city_team->hotline}}</a>
+                    </h3>
+                    <label for="support_email">Support-Email</label>
+                    <h3 class="hotline"><a
+                            href="mailto:{{$city->city_team->team_email}}">{{$city->city_team->team_email}}</a>
+                    </h3>
+                    <label for="support_email">Beschreibung</label>
+                    <h3 class="hotline">{{$city->city_team->description}}</h3>
+                    <p class="card-text">Text</p>
+                </div>
+            </div>
+
         </div>
     </div>
 @endsection
