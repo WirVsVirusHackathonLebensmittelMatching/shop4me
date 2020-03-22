570e1ab1e4e1c0cb47988e17625d66a748e62400
Tri Nhan Vu <trinhanvu@gmail.com>
2020-03-22 02:49:29 +0100

    add form fields for hotline and support email

--- resources/views/cities/edit.blade.php
+++ resources/views/cities/edit.blade.php
@@ -1,12 +1,28 @@
 @extends('layouts.app')
 @section('content')
-<div class="row">
-    <div class="col">
-        <h1>{{$city->city_name}}</h1>
-        <h1>{{$city->zip_code}}</h1>
-        <form method="POST" action="{{route('cities.edit', ['id' => $city->id])}}">
-            <input name="hotline" id="hotline" value="">
-        </form>
+    <div class="row">
+        <div class="col">
+            <span>Einkaufshelfer-Team für:</span>
+            <h1>{{$city->city_name}}</h1>
+            <h1>{{$city->zip_code}}</h1>
+        </div>
     </div>
-</div>
+    <div class="row">
+        <div class="col-8 mx-auto">
+            <form method="POST" action="{{route('cities.update', ['id' => $city->id])}}">
+                <div class="form-group">
+                    <label for="hotline">Deine Hotline</label>
+
+                    <input class="form-control form-control-lg" name="hotline" id="hotline" value="">
+                </div>
+                <div class="form-group">
+                    <label for="support_email">Deine Support-Email</label>
+
+                    <input class="form-control form-control-lg" name="support_email" id="support_email" value="">
+                </div>
+                <button type="submit" class="btn btn-success">Speichern</button>
+                <button type="submit" class="btn btn-dark">Speichern & Veröffentlichen</button>
+            </form>
+        </div>
+    </div>
 @endsection
