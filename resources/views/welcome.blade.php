1a8d6a926063aad0e34e8469c15fd0b3c9593a39
Tri Nhan Vu <trinhanvu@gmail.com>
2020-03-23 09:03:57 +0100

    add text and links to the google spreadsheet template and guide, add the pdf generator card to the front page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -132,6 +132,80 @@
     @endif
     <section>
         <div class="row mt-5">
+            <div class="col-lg-6 mx-auto">
+                <h2 class="text-center">Helferlein für die Helfer</h2>
+                <p class="text-center font-italic">Generiere deinen individuellen Abreiszettel und erhalte diesen direkt in deine E-Mail Inbox. Nutze es bitte nicht, um andere E-Mail Inboxen zu belästigen.</p>
+                <p>Hast du schon die Dokumentenvorlagen gesehen? Oder Das Google Spreadsheet, dass dir hilft deine Einkaufshelfer und Anfragen zu koordinieren?</p>
+                <p>Lese hierzu <a href="https://docs.google.com/document/d/1tVk9pQzUyl6pJJ3-Ugv9Wt5N-A0IYWh6RvJ75ktcpl0/edit#heading=h.swqhexp5uwg5" title="Google Spreadsheet Anleitung">die Anleitung</a> und nutze es. Wir freuen uns auf deine Rückmeldung, Verbesserungsvorschläge etc. Kopiere <a href="https://docs.google.com/spreadsheets/d/1TqwHgSbBtl8gAMNlaAAy7LOh4ldpv9cvDstIJ_aFYrQ/edit#gid=244255471" title="Google Spreadsheet Vorlage">die Vorlage</a> und leg los!</p>
+            </div>
+            <div class="col-lg-6">
+                <div class="card">
+                    <div class="card-header">
+                        PDF Abreisszeitel
+                    </div>
+                    <div class="card-body">
+                        <h4 class="card-title">Macht euch bekannt!</h4>
+                        <p class="card-text">Füge deine Daten ein und du erhälst per E-Mail deinen Abreisszettel mit
+                            vorgefertigten Formulierungen für deine Stadt. Falls du eingeloggt bist, werden die Felder
+                            automatisch befüllt.</p>
+                        @guest
+                            <form method="GET"
+                                  action="https://script.google.com/macros/s/AKfycbwBBlhhQJaEAxATAbS-_9-O94ZXTe8kOKN50t_TDxNwK-Hmrjk/exec?">
+                                <div class="form-group">
+                                    <label for="tel">Hotline</label>
+                                    <input class="form-control" name="tel" type="text" value="">
+                                </div>
+                                <div class="form-group">
+                                    <label for="mail">E-Mail</label>
+                                    <input class="form-control" name="mail" type="email" value="">
+                                </div>
+                                <div class="form-group">
+                                    <label for="mail">Stadt</label>
+                                    <input class="form-control" name="ort" type="text" value="">
+                                </div>
+                                <button class="btn btn-success" type="submit" name="action" value="google_pdf_render">PDF
+                                    generieren
+                                </button>
+                            </form>
+                        @endguest
+                        @auth
+                            <form method="GET"
+                                  action="https://script.google.com/macros/s/AKfycbwBBlhhQJaEAxATAbS-_9-O94ZXTe8kOKN50t_TDxNwK-Hmrjk/exec?">
+                                <div class="form-group">
+                                    <label for="tel">Hotline</label>
+                                    <input class="form-control" name="tel" type="text"
+                                           value="{{auth()->user()->city_team->hotline}}">
+                                </div>
+                                <div class="form-group">
+                                    <label for="mail">E-Mail</label>
+                                    <input class="form-control" name="mail" type="email"
+                                           value="{{auth()->user()->city_team->team_email}}">
+                                </div>
+                                <div class="form-group">
+                                    <label for="ort">Stadt</label>
+                                    <select name="ort" class="city-select" id="city-select" required>
+                                        @foreach(auth()->user()->city_team->cities as $city)
+                                            <option value="{{$city->city_name}}">{{$city->city_name}}, {{$city->zip_code}}</option>
+                                        @endforeach
+                                    </select>
+                                </div>
+                                <button class="btn btn-success" type="submit" name="action" value="google_pdf_render">PDF
+                                    generieren
+                                </button>
+                            </form>
+                        @endauth
+                    </div>
+                    <div class="card-footer text-muted">
+                        Es kann 1-3 Sekunden dauern. Habe etwas Geduld. <br>
+                        Der PDF-Generator läuft per Google Apps Script. Deine Daten werden hier an Google übermittelt. Was die damit noch machen? 🤷
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </section>
+    <section>
+        <div class="row mt-5">
             <div class="col-lg-5 mx-auto">
                 <h2 class="text-center">Die Story</h2>
                 <p class="text-center">Alles begann mit dem <a href="https://wirvsvirushackathon.org/"
